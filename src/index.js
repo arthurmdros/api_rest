26d192276eaf15232bccf4cc2467fd25aeb55c1e
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-30 23:24:11 -0300

    Removendo rota hello world

--- src/index.js
+++ src/index.js
@@ -6,8 +6,4 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.get('/', (req,res) => {
-    res.send('Hello World!');
-})
-
 app.listen(3333);
