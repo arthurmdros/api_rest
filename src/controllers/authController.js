52bad03904f8ea190246126c8073336662cd4309
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-31 00:02:26 -0300

    Adicionando restrição de usuário existe

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,12 @@ const router = express.Router();
 
 router.post('/register', async (req, res) => {
     try {
+        if(await User.findOne({ email })){
+            return res.status(400).send({ error: 'Usuário já existe'});
+        }
         const user = await User.create(req.body);
+
+        user.password = undefined;
 
         return res.send({ user });
     }catch(err){
