10cb71d6ca370f890045892f202b6814800a3d25
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-30 23:36:08 -0300

    Adicionando authController no arquivo de rotas

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./controllers/authController')(app);
 
 app.listen(3333);
