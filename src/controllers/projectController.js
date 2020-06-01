4855ac67444d86174b93fa35c83ea9f6ee8c50fc
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-31 23:54:08 -0300

    Importando middleware no projectController

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
+
+router.use(authMiddleware);
 
 router.get('/', (req, res) => {
     res.send({ ok: true });
