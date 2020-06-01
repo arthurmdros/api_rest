0b433542f6407338e4f402cbc2239304b584cd88
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-31 23:25:39 -0300

    Adicionando geração de token na rota de registro

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,6 +7,12 @@ const User = require('../models/user');
 
 const router = express.Router();
 
+function generateToken( params = {}){
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
+    });
+}
+
 router.post('/register', async (req, res) => {
     const { email } = req.body;
 
@@ -17,7 +23,10 @@ router.post('/register', async (req, res) => {
         const user = await User.create(req.body);
         user.password = undefined;
 
-        return res.send({ user });
+        return res.send({ 
+            user,
+            token: generateToken({ id: user.id }),
+        });
     }catch(err){
         return res.status(400).send({ error: 'Erro ao criar usuário' });
     }
@@ -37,11 +46,11 @@ router.post('/authenticate', async (req, res) => {
 
     user.password = undefined;
 
-    const token = jwt.sign({ id: user.id}, authConfig.secret, {
-        expiresIn: 86400,
-    });
 
-    res.send({ user, token });
+    res.send({ 
+        user, 
+        token: generateToken({ id: user.id }),
+    });
 });
 
 module.exports = app => app.use('/auth', router);
