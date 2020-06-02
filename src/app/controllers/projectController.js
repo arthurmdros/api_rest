72992f7251f03c1af6942ec3a40562aa2817971a
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-01 23:51:09 -0300

    Adicionando rota de criar um projeto

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authMiddleware = require('../middleware/auth');
 
 const Project = require('../models/project');
+const Task = require('../models/task');
 
 const router = express.Router();
 
@@ -25,6 +26,27 @@ router.get('/:projectId', async (req, res) => {
         return res.send({ project });
     }catch(err){
         return res.status(400).send({error: "Erro ao carregar projetos do usuário, tente novamente"});
+    }
+});
+
+router.post('/', async (req, res) => {
+    try{       
+        const { title, description, tasks }  = req.body;
+
+        const project = await Project.create({title, description , user: req.userId});
+
+        await Promise.all(tasks.map(async task => {            
+            const projectTask = new Task({...task, project: project._id});            
+            await projectTask.save();
+            
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
+
+        return res.send( {project} );
+    }catch(err){
+        return res.status(400).send({error: 'Erro ao cadastrar novo projeto, tente novamente'});
     }
 });
 
