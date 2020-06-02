2ebb29600717df5f08ef9b97df9716b0842a17bb
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-01 23:49:12 -0300

    Adicionando rota index no projectController

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth');
 
+const Project = require('../models/project');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId});
+router.get('/', async (req, res) => {
+    try{
+        const projects = await Project.find().populate(['user', 'tasks']);
+
+        return res.send({ projects });
+    }catch(err){
+        return res.status(400).send({error: "Erro ao carregar projetos, tente novamente"});
+    }
 });
+
 
 module.exports = app => app.use('/projects', router);
